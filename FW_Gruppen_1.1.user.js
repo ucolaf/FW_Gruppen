72807ac51c1b4c3390b7678a4d95f798175864ca
ucolaf <mordino@gmx.net>
2016-03-11 20:29:46 +0100

    Added files via upload

---
+++ FW_Gruppen_1.1.user.js
@@ -0,0 +1,69 @@
+// ==UserScript==
+// @name        Standort Gruppenchat Freewar
+// @namespace   Mordino
+// @description Postet den aktuellen Standort im Gruppenchat
+// @include     http://*.freewar.de/freewar/internal/*
+// @version     1
+// @grant       GM_getValue
+// @grant       GM_setValue
+// ==/UserScript==
+//alert("test");
+var frames = window.parent.frames;
+var Tags = document.getElementsByClassName('mainheader');
+for (var i = 0; i < Tags.length; i++) {
+  var tag = Tags[i];
+  var sendtext = tag.textContent;
+  var sendtext = sendtext.trim();
+}
+var Tags = document.getElementsByClassName('listusersrow');
+for (var i = 0; i < Tags.length; i++) {
+  var tag = Tags[i];
+  var sendtext2 = tag.textContent;
+  var sendtext2 = sendtext2.trim();
+  var npc = sendtext2.indexOf('(Gruppen-NPC)');
+  if (npc != - 1) {
+    var strleft = sendtext2.substring(0, npc);
+    var frames = window.parent.frames;
+    frames[3].document.getElementById('chat_text').value = strleft + ' (Gruppen-NPC)';
+    GM_setValue('ct', '1');
+  } 
+  else {
+    if (GM_getValue('ct') == '1') {
+      clearChat();
+    }
+  }
+  console.log(GM_getValue('ct') + npc);
+}
+var Tags = document.getElementsByClassName('personlistcaption');
+for (var i = 0; i < Tags.length; i++) {
+  var tag = Tags[i];
+  var sendtext3 = tag.textContent;
+  var sendtext3 = sendtext3.trim();
+  var npc = sendtext3.indexOf('Du siehst keine Person an diesem Ort');
+  if (npc != - 1) {
+    if (GM_getValue('ct') == '1') {
+      clearChat();
+    }
+  }
+}
+function clearChat() {
+  var frames = window.parent.frames;
+  frames[3].document.getElementById('chat_text').value = '';
+  GM_setValue('ct', '0');
+  // console.log('clearChat');
+}
+if (sendtext != null) {
+  var input = frames[3].document.createElement('input');
+  input.type = 'submit';
+  input.name = 'group';
+  input.className = 'input';
+  input.value = 'Chat';
+  input.onclick = sendChat;
+  input.setAttribute('style', 'font-size:12px;position:absolute;top:20px;right:20px;');
+  document.body.appendChild(input);
+}
+function sendChat() {
+  var frames = window.parent.frames;
+  //alert(frames[3].document.getElementById('group').value);
+  frames[3].document.getElementById('chat_text').value = sendtext;
+}
