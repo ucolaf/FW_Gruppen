3646a33e18606e1680b1f59cb512ec3c4222611d
ucolaf <mordino@gmx.net>
2018-01-29 08:45:38 +0100

    Update FW_Gruppen.user.js

--- FW_Gruppen.user.js
+++ FW_Gruppen.user.js
@@ -6,7 +6,7 @@
 // @include     *.freewar.de/freewar/internal/main.php
 // @version     1.2.5
 // @downloadURL https://github.com/ucolaf/FW_Gruppen/raw/master/FW_Gruppen.user.js
-// @updateURL   https://github.com/ucolaf/FW_Gruppen/raw/master/FW_Gruppen.user.js
+// @updateURL   https://raw.githubusercontent.com/ucolaf/FW_Gruppen/master/FW_Gruppen.user.js
 // @grant       GM_getValue
 // @grant       GM_setValue
 // ==/UserScript==
